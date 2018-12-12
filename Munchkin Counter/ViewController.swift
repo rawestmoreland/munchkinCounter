5a765dce8a476b1c903b40c540558c7a03047a08
rawestmoreland <rawestmoreland@gmail.com>
2018-12-12 17:28:07 -0600

    Add add-user icons and munchkin text on navbar

--- Munchkin Counter/ViewController.swift
+++ Munchkin Counter/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 
